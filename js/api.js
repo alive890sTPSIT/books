058ee0459674dd6a7a77da1c82ab621f5f079d92
arvr <acharig47@gmail.com>
2025-09-22 08:50:43 +0200

    lesson finished

--- js/api.js
+++ js/api.js
@@ -5,4 +5,5 @@ const res=await fetch("https://openlibrary.org/search.json?q=javascript");
 const data= await res.json();
 
 console.log(data.docs)
+const books=data.docs.map(d=>new Book(d.author_name,d.))
 }
